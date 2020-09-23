32c898640baddbc2677683e6f4ab80db2f19d857
Henil <39169439+henil113@users.noreply.github.com>
2020-09-23 19:55:45 -0400

    Final commit

--- contact.php
+++ contact.php
@@ -1,26 +1,21 @@
 <?php
 if(isset($_POST['submit'])) {
  
-    $email_from = "henilparikh113@gmail.com";
-    $email_to = "parik11n@uwindsor.ca";
-    $email_subject = "Inquiry";
- 
     $name = $_POST['name']; 
     $subject = $_POST['subject']; 
     $email = $_POST['email'];     
     $message = $_POST['message'];
     
-    $email_message = "Form details below.\n\n";
-    $email_message .= "Subject: ".$name."\n";
-    $email_message .= "Subject: ".$subject."\n";
-    $email_message .= "Email: ".$email."\n";
-    $email_message .= "Subject: ".$message."\n";
-    //echo $email_message;
-    
-    mail($email_to,$email_subject,$email_message,"From:".$email_from);
-    echo "<script>
-            alert('Mail Sent');
-            window.location.href='index.html';
-          </script>";
+    $to = 'parik11n@uwindsor.ca'
+    $subject='Form Submission'
+    $msg = "Name: ".$name."\n"."Subject: ".$subject."\n"."Wrote Following: "."\n\n".$message;
+    $headers = "From: ".$email
+
+    if(mail($to,$subject,$message,$headers)){
+        echo"<h1> Sent SUccesfully! </h1>";
+    }
+    else{
+        echo "Something went wrong!"; 
+    }
 }
 ?>
