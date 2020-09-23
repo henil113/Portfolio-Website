b220c120af4a8392f57f41644e7e7c250bf06329
Henil <39169439+henil113@users.noreply.github.com>
2020-09-23 19:41:34 -0400

    Final commit

---
+++ contact.php
@@ -0,0 +1,26 @@
+<?php
+if(isset($_POST['submit'])) {
+ 
+    $email_from = "henilparikh113@gmail.com";
+    $email_to = "parik11n@uwindsor.ca";
+    $email_subject = "Inquiry";
+ 
+    $name = $_POST['name']; 
+    $subject = $_POST['subject']; 
+    $email = $_POST['email'];     
+    $message = $_POST['message'];
+    
+    $email_message = "Form details below.\n\n";
+    $email_message .= "Subject: ".$name."\n";
+    $email_message .= "Subject: ".$subject."\n";
+    $email_message .= "Email: ".$email."\n";
+    $email_message .= "Subject: ".$message."\n";
+    //echo $email_message;
+    
+    mail($email_to,$email_subject,$email_message,"From:".$email_from);
+    echo "<script>
+            alert('Mail Sent');
+            window.location.href='index.html';
+          </script>";
+}
+?>
