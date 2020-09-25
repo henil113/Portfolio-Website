37fd5715bbbf9becbb881716b477c76b9e0761d2
Henil <39169439+henil113@users.noreply.github.com>
2020-09-24 20:43:31 -0400

    Final

--- contact.php
+++ contact.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_POST['submit'])) {
+if(isset($_POST['email'])) {
  
    
  
@@ -8,7 +8,7 @@ if(isset($_POST['submit'])) {
     $email = $_POST['email'];     
     $message = $_POST['message'];
 
-    $email_from = .$email;
+    $email_from = "henilparikh113@gmail.com";
     $email_to = "parik11n@uwindsor.ca";
     $email_subject = "Inquiry";
     
